f29690db84e974ccd3410e64c965d0316f60d7cf
gitnyasha <marshall.chikari@gmail.com>
2022-10-13 20:30:59 +0200

    .

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 const apiClient = axios.create({
-  baseURL: "https://sheltered-refuge-20729.herokuapp.com",
+  baseURL: "https://sheltered-refuge-20729.herokuapp.co.zw",
   headers: {
     "X-Requested-With": "XMLHttpRequest",
   },
   withCredentials: true,
 });
 
-const uri = "https://sheltered-refuge-20729.herokuapp.com/";
+const uri = "https://sheltered-refuge-20729.herokuapp.co.zw/";
 
 const backend = axios.create({
-  baseURL: "https://sheltered-refuge-20729.herokuapp.com",
-  // baseURL: "https://sheltered-refuge-20729.herokuapp.com",
+  baseURL: "https://sheltered-refuge-20729.herokuapp.co.zw",
 });
 export { apiClient, backend, uri };
