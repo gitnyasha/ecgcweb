9f30844b8f595198d0e4633e0f9b5283ebb0db6c
gitnyasha <marshall.chikari@gmail.com>
2022-11-29 11:55:36 +0200

    update backend link

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 const apiClient = axios.create({
-  baseURL: "https://dev.jorkshield.co.zw",
+  baseURL: "https://app.jorkshield.co.zw",
   headers: {
     "X-Requested-With": "XMLHttpRequest",
   },
   withCredentials: true,
 });
 
-const uri = "https://dev.jorkshield.co.zw/";
+const uri = "https://app.jorkshield.co.zw/";
 
 const backend = axios.create({
-  baseURL: "https://dev.jorkshield.co.zw",
+  baseURL: "https://app.jorkshield.co.zw",
 });
 export { apiClient, backend, uri };
