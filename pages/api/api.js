94a34c4edba88a577e3c68b0014379200bc0b9e5
gitnyasha <marshall.chikari@gmail.com>
2022-11-30 23:46:05 +0200

    .

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://app.jorkshield.co.zw",
   headers: {
     "X-Requested-With": "XMLHttpRequest",
   },
   withCredentials: true,
 });
 
-const uri = "http://localhost:8000/";
+const uri = "https://app.jorkshield.co.zw/";
 
 const backend = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://app.jorkshield.co.zw",
 });
 export { apiClient, backend, uri };
